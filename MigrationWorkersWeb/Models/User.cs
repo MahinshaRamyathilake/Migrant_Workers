c28b97a594736e2c89876e8cae3f1ca3768d37fa
MahinshaRamyathilake <mahiramyathilake@gmail.com>
2022-04-20 11:30:59 +0530

    First Commit

---
+++ MigrationWorkersWeb/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MigrationWorkersWeb.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string UserType { get; set; }
+        [Required]
+        public string status { get; set; }
+    }
+}
