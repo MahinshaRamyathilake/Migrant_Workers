de8fa17db2544f982dd9ac1eded5b3ddc0b365a3
MahinshaRamyathilake <mahiramyathilake@gmail.com>
2022-04-26 11:28:35 +0530

    added User add interface

---
+++ MigrationWorkersWeb/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using MigrationWorkersWeb.Data;
+using MigrationWorkersWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MigrationWorkersWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(User obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Users.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "User Created Successfully!";
+                return RedirectToAction("Index", "Home");
+            }
+            return View(obj);
+        }
+    }
+}
