6e52feb8601026dc83782286abe4a5b39773b072
MahinshaRamyathilake <mahiramyathilake@gmail.com>
2022-05-22 23:38:52 +0530

    Added SLFB_User_Email Model

---
+++ MigrationWorkersWeb/Models/SLFB_User_Email.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MigrationWorkersWeb.Models
+{
+    public class SLFB_User_Email
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int? SLFBUserID { get; set; }
+        [ForeignKey("SLFBUserID")]
+        public virtual SLFB_User SLFB_User { get; set; }
+
+        public string Email { get; set; }
+    }
+}
