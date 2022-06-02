2f4f90156ebd98a41552bb14d61c1d48372cc088
MahinshaRamyathilake <mahiramyathilake@gmail.com>
2022-06-02 16:26:23 +0530

    updated login

--- MigrationWorkersWeb/Controllers/AccountsController.cs
+++ MigrationWorkersWeb/Controllers/AccountsController.cs
@@ -1,15 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using System.Web;
+using MigrationWorkersWeb.Models;
+using System.Linq;
+using MigrationWorkersWeb.Data;
+
 
 namespace MigrationWorkersWeb.Controllers
 {
     public class AccountsController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public AccountsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(Login credentials)
+        {
+            bool userExist = _db.Users.Any(u => u.UserName == credentials.UserName && u.Password == credentials.Password);
+            User user = _db.Users.FirstOrDefault(u => u.UserName == credentials.UserName && u.Password == credentials.Password);
+
+            if (userExist)
+            {
+                
+            }
+            return View();
+        }
+
         public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(User obj)
         {
             return View();
         }
