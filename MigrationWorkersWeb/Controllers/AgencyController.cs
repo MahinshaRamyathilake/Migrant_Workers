6d592459835f9914a35dc799f746219e33bea772
MahinshaRamyathilake <mahiramyathilake@gmail.com>
2022-06-09 21:52:54 +0530

    updated

--- MigrationWorkersWeb/Controllers/AgencyController.cs
+++ MigrationWorkersWeb/Controllers/AgencyController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MigrationWorkersWeb.Data;
+using MigrationWorkersWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MigrationWorkersWeb.Controllers
 {
     public class AgencyController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public AgencyController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            var agencies = _db.Agencies;
+            return View(agencies);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Agency obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Agencies.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Agency Created Successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
--- MigrationWorkersWeb/Controllers/EmbassyController.cs
+++ MigrationWorkersWeb/Controllers/EmbassyController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MigrationWorkersWeb.Data;
+using MigrationWorkersWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MigrationWorkersWeb.Controllers
 {
     public class EmbassyController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public EmbassyController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            var embassies = _db.Embassies;
+            return View(embassies);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Embassy obj)
+        {
+            if (ModelState.IsValid)
+            { 
+                _db.Embassies.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Embassy Created Successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
         }
     }
 }
